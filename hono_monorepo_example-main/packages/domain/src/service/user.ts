471e68643c0a56c82802b811a1234f4097bef4d2
Arachaphon Klinchuen <arachaphonklinchuen@gmail.com>
2025-12-16 10:18:19 +0700

    .

--- hono_monorepo_example-main/packages/domain/src/service/user.ts
+++
@@ -1,7 +0,0 @@
-import type {UserCreateDto, UserDto} from "../dto/user.dto.js";
-
-export interface IUserService {
-
-    findAllUser() : Promise<UserDto[]>
-    create(userDto : UserCreateDto) : Promise<UserDto>
-}
--- hono_monorepo_example-main/packages/domain/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import {z, type ZodType} from 'zod';
-
-export function createResponseSchema<T extends ZodType<any>>(dataSchema: T) {
-    return z.object({
-        message: z.string(),
-        data: z.union([dataSchema, z.array(dataSchema)]),
-    });
-}
